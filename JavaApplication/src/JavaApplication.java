e0b062eba44eba965989e83588d710cd8b3826bd
liangzhao <leonchaoz@gmail.com>
2015-04-26 23:04:36 +1000

    123

--- JavaApplication/src/JavaApplication.java
+++ JavaApplication/src/JavaApplication.java
@@ -3,13 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication;
+
 
 /**
  *
  * @author liang
- */
-public class JavaApplication {
+ */public class JavaApplication {
 
     /**
      * @param args the command line arguments
